5c177aca68cfc606f9b37eb74a2080aeb344e696
LordPrimusTL <MichealAkinwonmi@gmail.com>
2017-10-05 13:08:57 +0100

    *User Investing Cre complete

--- resources/views/Partials/_message.blade.php
+++ resources/views/Partials/_message.blade.php
@@ -1,14 +1,14 @@
 @if(Session::has('success'))
     <div class="alert alert-dismissible alert-success" style="margin-left: 10px;">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Success:</strong> {{Session::get('success')}}
+        <strong><i class="fa fa-thumbs-up"></i> Success:</strong> {{Session::get('success')}}
     </div>
 @endif
 
 @if(Session::has('error'))
     <div class="alert alert-dismissible alert-danger" style="margin-left: 10px;">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong style="margin-left: 10px;">Error:</strong> {{Session::get('error')}}
+        <strong style="margin-left: 10px;"><i class="fa fa-crosshairs"></i>Error:</strong> {{Session::get('error')}}
     </div>
 @endif
 @if (count($errors) > 0)
